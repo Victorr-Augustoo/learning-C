d5d3500fbc7374cb6249baac962932c41d550bd7
Victor <victoraugusto987@gmail.com>
2022-10-30 21:58:27 -0300

    commit

---
+++ structs/stru_ex2.c
@@ -0,0 +1,34 @@
+/*
+Implemente um programa que leia o nome, a idade e o endereço de uma pessoa e armazene os dados em uma estrutura.
+*/
+// not working properly
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct{
+    char nome[20];
+    int idade;
+    char endereco[50];
+} cadastro;
+
+int main(){
+    cadastro c;
+
+    printf("digite o nome: \n");
+    gets(c.nome);
+
+    printf("digite a idade: \n");
+    scanf("%i", &c.idade);
+
+    printf("digite o endereco: \n");
+    gets(c.endereco);
+
+    printf("informacoes digitadas: \n");
+    printf("nome: %s\n", c.nome);
+    printf("idade: %i\n", c.idade);
+    printf("endereco: %s\n", c.endereco);
+
+    system("pause");
+    return 0;
+}
